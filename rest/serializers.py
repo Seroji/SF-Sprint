802a9ca07b61120f2f7035ed6523dae154b5c7c4
Seroji <gamexr6@mail.ru>
2023-05-28 20:54:39 +0300

    Coords, Level, Image, Pereval serizliers added

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,4 +1,37 @@
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
+
+from .models import (Coords,
+                     Level,
+                     Image,
+                     PerevalAdded)
 
 
-class 
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = '__all__'
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Image
+        fields = '__all__'
+
+
+class PerevalSerializers(serializers.ModelSerializer):
+    coords = CoordsSerializer(many=True)
+    level = LevelSerializer(many=True)
+    images = ImageSerializer(many=True)
+
+    class Meta:
+        model = PerevalAdded
+        fields = '__all__'
+
+    # def create(self, validated_data):
